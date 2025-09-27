5dae72c32fb0cd1e170b78e90f51916652e034d8
v26828285 <v26828285@gmail.com>
2025-09-27 12:00:23 +0800

    ..

---
+++ lesson5/lesson5-3.py
@@ -0,0 +1,11 @@
+import random
+import gradio as gr
+
+def random_response(message, history):
+    return random.choice(["Yes", "No"])
+
+demo = gr.ChatInterface(random_response, type="messages", autofocus=False)
+
+if __name__ == "__main__":
+    demo.launch()
+#chatbot
